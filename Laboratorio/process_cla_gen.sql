47310ede8b29ffc6738e6c399d2234494f86d0c3
Luis Henrique Alves de Sousa <luishenrique.alves@ikasle.egibide.org>
2021-05-13 18:09:23 +0200

    Procesos xml

---
+++ Laboratorio/process_cla_gen.sql
@@ -0,0 +1,45 @@
+create or replace procedure clasi_general (pIdTorneo torneos.idtorneo%type)
+IS
+    qryctx dbms_xmlgen.cxHandle;
+    result clob;
+begin
+    qryctx := dbms_xmlgen.newContext(
+        select jornada_gen_tipo(idjornada,
+                                fecha,
+                                cast(multiset
+                                (select idequipo,
+                                        (select sum(nvl(puntuacion, 0))
+                                        from partidos_jugados pj
+                                        where pj.idequipo = equi.idequipo
+                                        and idpartido = (select idpartido
+                                                        from partidos
+                                                        where idjornada = (select idjornada
+                                                                            from jornadas jor
+                                                                            where jor.idtorneo = j.idtorneo)))
+                                        as puntuacion,
+                                        nombre,
+                                        pais,
+                                        cast(multiset(select idpersona,
+                                                            nickname,
+                                                            rol
+                                                    from jugadores jug
+                                                    where jug.idequipo = equi.idequipo)
+                                                    as jugador)
+                                from equipos equi)
+                                as equipo))
+        as jornada
+        from jornadas j
+        where idjornada = (select max(idjornada)
+                            from jornadas)
+    );
+
+    DBMS_XMLGEN.setRowSetTag(qryCtx, 'clasificacion');
+    DBMS_XMLGEN.setRowTag(qryCtx, 'ultima_jornada');
+
+    result := dbms_xmlgen.getxml(qryctx);
+    insert into clagenresult (resultset) values (result);
+
+    dbms_xmlgen.closecontext(qryctx);
+exception
+
+end;
