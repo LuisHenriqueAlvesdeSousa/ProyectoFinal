3e4bfb4b6ef1ef7196f67740c30679f6b84257e6
Luis H <luishenrique.alves@ikasle.egibide.org>
2021-05-14 10:59:01 +0200

    UML, tabla jefe, tabla persona

--- Fase-3/TorneoES/src/BD/tablaPersonas.java
+++ Fase-3/TorneoES/src/BD/tablaPersonas.java
@@ -1,14 +1,217 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package BD;
+
+import UML.Persona;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 /**
  *
  * @author 1GDAW04
  */
 public class tablaPersonas {
+    private static Connection con;
     
+    public static void crearPersona(Persona p) throws Exception{
+        BaseDatos.conectar();
+        con = BaseDatos.getCon();
+        
+        String plantilla = "INSERT INTO PERSONAS("
+                                                + " DNI,"
+                                                + " NOMBRE,"
+                                                + " APELLIDO,"
+                                                + " FECHANACIMIENTO,"
+                                                + " SUELDO,"
+                                                + " TELEFONO,"
+                                                + " FECHACONTRATO,"
+                                                + " NACIONALIDAD) "
+                          + "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+        PreparedStatement ps = con.prepareStatement(plantilla);
+        ps.setString(1, p.getDni());
+        ps.setString(2, p.getNombre());
+        ps.setString(3, p.getApellido());
+        ps.setDate(4, java.sql.Date.valueOf(p.getFechaNacimiento()));
+        ps.setDouble(5, p.getSueldo());
+        ps.setString(6, p.getTelefono());
+        ps.setDate(7, java.sql.Date.valueOf(p.getFechaContrato()));
+        ps.setString(8, p.getNacionalidad());
+        
+        int n = ps.executeUpdate();
+        
+        if(n != 1)
+            throw new Exception("Se ha insertado más de una Persona.");
+        
+        System.out.println("Persona insertado con exito.");
+        
+        BaseDatos.desconectar();
+    }
+    
+    public static void modPersona(Persona p) throws Exception{
+        BaseDatos.conectar();
+        con = BaseDatos.getCon();
+        
+        String plantilla = "UPDATE PERSONAS SET"
+                                                + " DNI = ?,"
+                                                + " NOMBRE = ?,"
+                                                + " APELLIDO = ?,"
+                                                + " FECHANACIMIENTO = ?,"
+                                                + " SUELDO = ?,"
+                                                + " TELEFONO = ?,"
+                                                + " FECHACONTRATO = ?,"
+                                                + " NACIONALIDAD = ?"
+                          + "WHERE IDPERSONA = ?;";
+        PreparedStatement ps = con.prepareStatement(plantilla);
+        ps.setString(1, p.getDni());
+        ps.setString(2, p.getNombre());
+        ps.setString(3, p.getApellido());
+        ps.setDate(4, java.sql.Date.valueOf(p.getFechaNacimiento()));
+        ps.setDouble(5, p.getSueldo());
+        ps.setString(6, p.getTelefono());
+        ps.setDate(7, java.sql.Date.valueOf(p.getFechaContrato()));
+        ps.setString(8, p.getNacionalidad());
+        ps.setInt(9, p.getIdPersona());
+        
+        int n = ps.executeUpdate();
+        
+        if(n != 1)
+            throw new Exception("Se ha actualizado más de una Persona.");
+        
+        System.out.println("Persona actualizada con exito.");
+        
+        BaseDatos.desconectar();
+    }
+    
+    public static void eliminarPersona(Persona p) throws Exception{
+        BaseDatos.conectar();
+        con = BaseDatos.getCon();
+        
+        String plantilla = "DELETE FROM PERSONAS WHERE IDPERSONA = ?;";
+        PreparedStatement ps = con.prepareStatement(plantilla);
+        ps.setInt(1, p.getIdPersona());
+        
+        int n = ps.executeUpdate();
+        
+        if(n != 1)
+            throw new Exception("Se ha eliminado más de una Persona.");
+        
+        System.out.println("Se ha eliminado la Persona con exito.");
+        BaseDatos.desconectar();
+    }
+    
+    public static ArrayList<Persona> allPersona(Persona p) throws Exception{
+        BaseDatos.conectar();
+        con = BaseDatos.getCon();
+        
+        String plantilla = "SELECT * FROM PERSONAS;";
+        PreparedStatement ps = con.prepareStatement(plantilla);
+        
+        ResultSet resultado = ps.executeQuery();
+        
+        Persona personaActual = new Persona();
+        ArrayList<Persona> listaPersonas = new ArrayList();
+        
+        if(resultado == null){
+            listaPersonas = null;
+            System.out.println("No se ha encontrado Personas en la BD.");
+        }
+        else{
+            while(resultado.next()){
+                personaActual.setIdPersona(resultado.getInt("IDPERSONA"));
+                personaActual.setDni(resultado.getString("DNI"));
+                personaActual.setNombre(resultado.getString("NOMBRE"));
+                personaActual.setApellido(resultado.getString("APELLIDO"));
+                personaActual.setFechaNacimiento(
+                        resultado.getDate("FECHANACIMIENTO").toLocalDate());
+                personaActual.setSueldo(resultado.getDouble("SUELDO"));
+                personaActual.setTelefono(resultado.getString("TELEFONO"));
+                personaActual.setFechaContrato(
+                        resultado.getDate("FECHACONTRATO").toLocalDate());
+                personaActual.setFechaFinContrato(
+                        resultado.getDate("FECHAFINCONTRATO").toLocalDate());
+                personaActual.setNacionalidad(
+                        resultado.getString("NACIONALIDAD"));
+                listaPersonas.add(personaActual);
+            }
+            System.out.println("Todos los jefes selecionados con exito.");
+        }
+        
+        BaseDatos.desconectar();
+        return listaPersonas;
+    }
+    
+    public static Persona PersonaByDni(Persona p) throws Exception{
+        BaseDatos.conectar();
+        con = BaseDatos.getCon();
+        
+        String plantilla = "SELECT * FROM PERSONAS WHERE DNI = ?;";
+        PreparedStatement ps = con.prepareStatement(plantilla);
+        ps.setString(1, p.getDni());
+        
+        ResultSet resultado = ps.executeQuery();
+        
+        Persona personaActual = new Persona();
+        
+        if(resultado == null){
+            personaActual = null;
+        }
+        else{
+            resultado.next();
+            personaActual.setIdPersona(resultado.getInt("IDPERSONA"));
+                personaActual.setDni(resultado.getString("DNI"));
+                personaActual.setNombre(resultado.getString("NOMBRE"));
+                personaActual.setApellido(resultado.getString("APELLIDO"));
+                personaActual.setFechaNacimiento(
+                        resultado.getDate("FECHANACIMIENTO").toLocalDate());
+                personaActual.setSueldo(resultado.getDouble("SUELDO"));
+                personaActual.setTelefono(resultado.getString("TELEFONO"));
+                personaActual.setFechaContrato(
+                        resultado.getDate("FECHACONTRATO").toLocalDate());
+                personaActual.setFechaFinContrato(
+                        resultado.getDate("FECHAFINCONTRATO").toLocalDate());
+                personaActual.setNacionalidad(
+                        resultado.getString("NACIONALIDAD"));
+        }
+        
+        BaseDatos.desconectar();
+        return personaActual;
+    }
+    
+    public static Persona PersonaById(Persona p) throws Exception{
+        BaseDatos.conectar();
+        con = BaseDatos.getCon();
+        
+        String plantilla = "SELECT * FROM PERSONAS WHERE IDPERSONA = ?;";
+        PreparedStatement ps = con.prepareStatement(plantilla);
+        ps.setInt(1, p.getIdPersona());
+        
+        ResultSet resultado = ps.executeQuery();
+        
+        Persona personaActual = new Persona();
+        
+        if(resultado == null){
+            personaActual = null;
+        }
+        else{
+            resultado.next();
+            personaActual.setIdPersona(resultado.getInt("IDPERSONA"));
+                personaActual.setDni(resultado.getString("DNI"));
+                personaActual.setNombre(resultado.getString("NOMBRE"));
+                personaActual.setApellido(resultado.getString("APELLIDO"));
+                personaActual.setFechaNacimiento(
+                        resultado.getDate("FECHANACIMIENTO").toLocalDate());
+                personaActual.setSueldo(resultado.getDouble("SUELDO"));
+                personaActual.setTelefono(resultado.getString("TELEFONO"));
+                personaActual.setFechaContrato(
+                        resultado.getDate("FECHACONTRATO").toLocalDate());
+                personaActual.setFechaFinContrato(
+                        resultado.getDate("FECHAFINCONTRATO").toLocalDate());
+                personaActual.setNacionalidad(
+                        resultado.getString("NACIONALIDAD"));
+        }
+        
+        BaseDatos.desconectar();
+        return personaActual;
+    }
 }
